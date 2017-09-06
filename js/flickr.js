e6050e2c28df4dfc9caef5482c31b1dac9478e8d
Yang Wang <Yang.Wang04@gmail.com>
2017-09-06 14:54:07 +1000

    implement call back function to handle Ajax request with flickerAPI

--- js/flickr.js
+++ js/flickr.js
@@ -1,9 +1,21 @@
-var url = "https://api.flickr.com/services/feeds/photos_public.gne?format=json";
 $('button').on('click',function(){
-  $('button').removeClass("selected");
-  $(this).addClass("selected");
-
-
-
-
+  $('button').removeClass('selected');
+  $(this).addClass('selected');
+  var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+      animal = $(this).text(),
+      flickerOptions = {
+        tags: animal,  //filter data set
+        format: "json"
+      },
+      displayPhotos = function(response){
+        var statusHtml = '<ul>';
+        $.each(response.items,function(index,photo){
+          statusHtml += '<li class="grid-25 tablet-grid-50">';
+          statusHtml += '<a href="' + photo.link + '"class ="image">';
+          statusHtml += '<img src="' + photo.media.m + '"></a></li>';
+        });
+        statusHtml += '</ul>';
+        $('#photos').html(statusHtml);
+      };
+  $.getJSON(flickerAPI,flickerOptions,displayPhotos);
 });
