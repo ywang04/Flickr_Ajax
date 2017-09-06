7fb7ef9f0a70bc735043d8cccc731c019dfe97b6
Yang Wang <Yang.Wang04@gmail.com>
2017-09-06 10:21:41 +1000

    initial commit

---
+++ js/flickr.js
@@ -0,0 +1,9 @@
+var url = "https://api.flickr.com/services/feeds/photos_public.gne?format=json";
+$('button').on('click',function(){
+  $('button').removeClass("selected");
+  $(this).addClass("selected");
+
+
+
+
+});
