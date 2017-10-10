d4d418a090a9be18ce053f0a65070067f42bde3c
Yang Wang <Yang.Wang04@gmail.com>
2017-10-10 11:48:21 +1100

    Refactor the flicker.js with binding event on form

--- js/flickr.js
+++ js/flickr.js
@@ -1,10 +1,34 @@
-$('button').on('click',function(){
-  $('button').removeClass('selected');
-  $(this).addClass('selected');
+// $('button').on('click',function(){
+//   $('button').removeClass('selected');
+//   $(this).addClass('selected');
+//   var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+//       animal = $(this).text(),
+//       flickerOptions = {
+//         tags: animal,  //filter data set
+//         format: "json"
+//       },
+      // displayPhotos = function(response){
+      //   var statusHtml = '<ul>';
+      //   $.each(response.items,function(index,photo){
+      //     statusHtml += '<li class="grid-25 tablet-grid-50">';
+      //     statusHtml += '<a href="' + photo.link + '"class ="image">';
+      //     statusHtml += '<img src="' + photo.media.m + '"></a></li>';
+      //   });
+      //   statusHtml += '</ul>';
+      //   $('#photos').html(statusHtml);
+      // };
+//   $.getJSON(flickerAPI,flickerOptions,displayPhotos);
+// });
+
+$('form').submit(function(event){
+  event.preventDefault();
+  var $searchField = $('#search');
+  var $submitButton = $('#submit');
+  //the Ajax part
   var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-      animal = $(this).text(),
+      animal = $searchField.val();
       flickerOptions = {
-        tags: animal,  //filter data set
+        tags: animal,
         format: "json"
       },
       displayPhotos = function(response){
